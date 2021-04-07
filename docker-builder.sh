1d16aed9b77ddb6d92cd09f25c38b81477eae852
Vassiliy Yegorov <vasyakrg@gmail.com>
2021-04-07 12:30:20 +0700

    add push

--- docker-builder.sh
+++ docker-builder.sh
@@ -10,3 +10,6 @@ docker build -f Dockerfile-base -t vasyakrg/selectel-hc:base .
 
 docker build -t vasyakrg/selectel-hc:latest .
 docker tag vasyakrg/selectel-hc:latest vasyakrg/selectel-hc:$(cat VERSION)
+
+docker push vasyakrg/selectel-hc:latest
+docker push vasyakrg/selectel-hc:$(cat VERSION)
